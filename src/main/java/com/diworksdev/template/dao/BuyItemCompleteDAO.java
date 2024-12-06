22787ce5ab8a2288af1448ee8f07c247af099fb9
h-satoshi <h8-shnagasaki@au.com>
2024-12-07 02:29:15 +0900

    12/7

--- src/main/java/com/diworksdev/template/dao/BuyItemCompleteDAO.java
+++ src/main/java/com/diworksdev/template/dao/BuyItemCompleteDAO.java
@@ -12,11 +12,13 @@ public class BuyItemCompleteDAO {
 	private DateUtil dateUtil = new DateUtil();
 	
 	private String sql = "INSERT INTO user_buy_item_transaction(item_transaction_id, "
-							+ "total_price, total_count, user_master_id, pay, insert_date) VALUES(?,?,?,?,?,?)";
+						+ "total_price, total_count, user_master_id, pay, insert_date) "
+						+ "VALUES(?,?,?,?,?,?)";
 	
 	
 	public  void buyItemInfo(String item_transaction_id, String user_master_id,
-								String total_price, String total_count, String pay) throws SQLException {
+							 String total_price, String total_count, String pay) 
+							 throws SQLException {
 		
 		DBConnector dbConnector = new DBConnector();
 		
